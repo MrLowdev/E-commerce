b888e0fdaa73a06ee6d6e68c32050a4263874bfa
kingsuk <kingsuk@github.com>
2024-03-07 17:23:35 +0530

    feat - create add to cart function

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -23,6 +23,7 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
+      onClick={onClick}
       className={`disabled:opacity-70 disabled:cursor-not-allowed rounded-md hover:opacity-80 w-full transition border-slate-700 flex items-center justify-center gap-2 ${
         outline ? "bg-white" : "bg-slate-700"
       } ${outline ? "text-slate-700" : "text-white"} ${
