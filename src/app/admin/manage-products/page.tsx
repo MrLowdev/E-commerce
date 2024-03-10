b49a283da4aacf643cd978e00cc268433c7fd4d8
kingsuk <kingsuk@github.com>
2024-03-10 11:50:39 +0530

    feat- create mange product page

--- src/app/admin/manage-products/page.tsx
+++ src/app/admin/manage-products/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import getProducts from "@/actions/getProducts";
+import Container from "@/components/Container";
+import NullData from "@/components/NullData";
+import ManageProductsClient from "@/components/admin/Manage-products/ManageProductsClient";
 
-const ManageProducts = () => {
-  return <div>ManageProducts</div>;
+const ManageProducts = async () => {
+  const products = await getProducts({ category: null });
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Oops! Access Denied" />;
+  }
+  return (
+    <Container>
+      <ManageProductsClient products={products} />
+    </Container>
+  );
 };
 
 export default ManageProducts;
